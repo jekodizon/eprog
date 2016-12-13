4a170c0a6e549dfd10b4e3df266ed02d84ccef34
Ayrton Ari H. Dizon <jdizon12@ymail.com>
2016-12-13 08:58:58 +0800

    Create lab1a.c

---
+++ lab1a.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float a,b,c,d;
+
+printf("enter value for a\n");
+scanf("%f", &a);
+printf("enter value for b\n");
+scanf("%f", &b);
+printf("enter value for c\n");
+
+scanf("%f", &c);
+
+printf("%f", d = (a+b+c)/3);
+
+
+
+}
