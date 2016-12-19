1f524581b8701583714cf00d8483939d507cdc86
Ayrton Ari H. Dizon <jdizon12@ymail.com>
2016-12-20 07:52:08 +0800

    Create if_play.c

---
+++ if_play.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+	int n;
+	
+	printf("enter number:\n ");
+	scanf("%d", &n);
+	
+if (n % 3 ==0){
+	printf("Ed ");	
+}
+if (n%5==0){
+	printf("pogi");
+}
+}
