6ddc9f765a94a0a269ee1cd4116fdef6fd4ba498
Ayrton Ari H. Dizon <jdizon12@ymail.com>
2016-12-20 08:16:30 +0800

    Update if_play.c

--- if_play.c
+++ if_play.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include<math.h>
+
 
 int main(){
 	int n;
@@ -7,11 +7,11 @@ int main(){
 	printf("enter number:\n ");
 	scanf("%d", &n);
 	
-	if (n % 3 ==0){
+	if (n % 5 ==0){
 		printf("Ed ");	
 	}
 	
-	if (n%5==0){
+	if (n% 3==0){
 		printf("pogi");
 	}
 }
