20715184c4d4bfc6730033b298dcf99059b8e6bf
Ayrton Ari H. Dizon <jdizon12@ymail.com>
2016-12-20 08:40:44 +0800

    Create date.c

---
+++ date.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+
+
+int main(){
+	int n, d;
+	
+	printf("enter month 1-12:\n ");
+	scanf("%d", &n);
+	
+	printf("enter day 1-7:\n ");
+	scanf("%d", &d);
+	
+	if (n==1){
+		printf("January ");	
+	}
+	
+	if (n==2){
+		printf("Febuary");
+	}
+		if (n==3){
+		printf("March");
+	}
+		if (n==4){
+		printf("April");
+	}
+		if (n==5){
+		printf("May");
+	}
+		if (n==6){
+		printf("June");
+	}
+	if (n==7){
+		printf("July");
+	}
+	if (n==8){
+		printf("August");
+	}
+	if (n==9){
+		printf("September");
+	}
+	if (n==10){
+		printf("October");
+	}
+		if (n==11){
+		printf("November");
+	}
+		if (n==12){
+		printf("December");
+	}
+	
+	
+	if (d==1){
+		printf("Sunday");
+	}
+	
+	if (d==2){
+		printf("Monday");
+	}
+	if (d==3){
+		printf("Tuesday");
+	}
+	if (d==4){
+		printf("Wednesday");
+	}
+	if (d==5){
+		printf("Thursday");
+	}
+	if (d==6){
+		printf("Friday");
+	}
+	if (d==7){
+		printf("Saturday");
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
